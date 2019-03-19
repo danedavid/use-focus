07d52b7b0efc162b7aa57e144ffb6e429a7b369a
Dane David <dane@surveysparrow.com>
2019-03-19 09:03:11 +0530

    Updates

---
+++ precache-manifest.5548c099a71a8ee6547fef99819135f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0c5ce0d5d429e71940e",
+    "url": "/use-focus/static/js/runtime~main.5b668e6d.js"
+  },
+  {
+    "revision": "a0e8770187a31ea497ac",
+    "url": "/use-focus/static/js/main.f6bd299c.chunk.js"
+  },
+  {
+    "revision": "2d58b08dc98124429ae8",
+    "url": "/use-focus/static/js/2.43f2b6ab.chunk.js"
+  },
+  {
+    "revision": "a0e8770187a31ea497ac",
+    "url": "/use-focus/static/css/main.f0820868.chunk.css"
+  },
+  {
+    "revision": "26516eb5311dd6fb2452b2af6f58d217",
+    "url": "/use-focus/index.html"
+  }
+];
